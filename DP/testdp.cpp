f5cf940b997195153ba29373c523091aa42bb56b
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-02 18:27:19 -0800

    Create testdp.cpp

---
+++ DP/testdp.cpp
@@ -0,0 +1,34 @@
+#include "longestPalindrome5.hpp"
+#include "countingBits338.hpp"
+#include "wordBreak139.hpp"
+#include "maxProductSubArray152.hpp"
+#include "triangle120.hpp"
+#include "uniquePath62.hpp"
+#include "minPathSum64.hpp"
+#include "decodeWays91.hpp"
+#include "arithmetricSlice413.hpp"
+#include "uglyNumber264.hpp"
+#include "coinChange322.hpp"
+#include "longestPalindromeSub516.hpp"
+#include "deleteAndEarn740.hpp"
+#include "largestDivisibleSubset368.hpp"
+#include "findLength718.hpp"
+#include "minimumDeleteSum712.hpp"
+int main()
+{
+  //LongestPalindrome5 test; test.test();
+  //countingBits338 test; test.test();
+  //wordBreak139 test; test.test();
+  //maxProductSubArray152 test; test.test();
+  //triangle120 test; test.test();
+  //uniquePath62 test; test.test();	
+  //minPathSum64 test; test.test();
+  //decodeWays91 test; test.test();
+  //arithmetricSlice413 test; test.test();	
+  //UglyNum264 test; test.test();	
+  //coinChange322 test; test.test();
+  //longestPalindromeSub516 test; test.test();	
+  //largestDivisibleSubset368 test; test.test();
+  //findLength718 test; test.test();
+  minimumDeleteSum712 test; test.test();
+}
