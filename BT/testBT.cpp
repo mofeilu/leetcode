bfbd87280962d7de1724ceae7ca286aeb00e5bd6
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:52:05 +0000

    Create testBT.cpp

---
+++ BT/testBT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include "utils.hpp"
+using namespace std;
+#include "LetterCombPhoneNum17.hpp"
+int main()
+{
+  LetterCombPhoneNum17 test; test.test();
+}
