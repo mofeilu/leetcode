9fed91156c974e2bf4b20f54b95cde3a3432f638
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:14:23 +0000

    Create testLinklist.cpp

---
+++ Linklist/testLinklist.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include "utils.hpp"
+using namespace std;
+#include "IntersectionOfTwoLinkedLists160.hpp"
+#include "SwapNodesInPairs24.hpp"
+#include "LinkedListCycleII142.hpp"
+#include "RemoveNthNodeFromEndOfList19.hpp"
+#include "RemoveDuplicatesfromSortedList83.hpp"
+#include "AddTwoNumbersII445.hpp"
+#include "RmLinkedListElem203.hpp"
+#include "ReverseLinkedListII92.hpp"
+#include "PartitionList86.hpp"
+#include "RotateList61.hpp"
+#include "ReorderList143.hpp"
+int main()
+{
+  //IntersectionOfTwoLinkedLists160 t; t.test();
+  //SwapNodesInPairs24 t; t.test();
+  //LinkedListCycleII142 t; t.test();
+  //RemoveNthNodeFromEndOfList19 t; t.test();
+  //RemoveDuplicatesfromSortedList83 t; t.test();
+  //AddTwoNumbersII445 t; t.test();
+  //RmLinkedListElem203 t; t.test();
+  //ReverseLinkedListII92 t; t.test();
+  //PartitionList86 t; t.test();
+  //RotateList61 t; t.test();
+   ReorderList143 t; t.test();
+
+}
