3044c89f361d94a8d62e7d7806ccd9052f24d8c8
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 17:57:00 +0000

    Create testmath.cpp

---
+++ math/testmath.cpp
@@ -0,0 +1,34 @@
+#include "utils.hpp"
+#include <iostream>
+
+
+#include "p223RectArea.hpp"
+#include "p319BulbSwitch.hpp"
+#include "pow50.hpp"
+#include "devideTwoInt29.hpp"
+#include "MaxPointsOnLine149.hpp"
+#include "perfectSquare279.hpp"
+#include "intToRoman12.hpp"
+#include "strToInt8.hpp"
+#include "MultStr43.hpp"
+#include "CountPrime204.hpp"
+#include "NumOfDigitOne233.hpp"
+int main()
+{
+
+  //std::cout << "hello world\n";
+
+  //p223RectArea test; test.test();
+  //BulbSwitch319 test; test.test();
+  //pow50 test; test.test();
+  //std::cout << (-3)/2 << ' ' << (-3)%2 << std::endl;
+
+  //devideTwoInt29 test; test.test();
+  //MaxPointsOnLine149 test; test.test();
+  // PerfectSquare279 test; test.test();
+  //IntToRoman12 test; test.test();	
+  //StrToInt8 test; test.test();
+  //MultStr43 test; test.test();
+  //CountPrime204 test; test.test();
+  NumOfDigitOne233 test; test.test();
+}
