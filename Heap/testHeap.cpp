34d0529a319e1df4a10ab0ef2312b13fcb3d713f
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:49:50 +0000

    Create testHeap.cpp

---
+++ Heap/testHeap.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include "utils.hpp"
+using namespace std;
+#include "SortCharByFreq451.hpp"
+#include "SuperUglyNumber313.hpp"
+#include "TopKFrequentWords692.hpp"
+#include "FindKPairswithSmallestSums373.hpp"
+#include "SplitArrayConsSubseq659.hpp"
+#include "ReorganizeString767.hpp"
+int main()
+{
+  //SortCharByFreq451 t; t.test();
+  //SuperUglyNumber313 t; t.test();
+  //TopKFrequentWords692 t; t.test();
+  //FindKPairswithSmallestSums373 t; t.test();
+  //SplitArrayConsSubseq659 t; t.test();
+  //ReorganizeString767 t; t.test(); 
+}
