6954240d450da465353836c28170a42901b753c3
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 17:47:12 +0000

    Create testDFS.cpp

---
+++ DFSandBFS/testDFS.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include "utils.hpp"
+#include "NumberOfIslands200.hpp"
+#include "FriendCircles547.hpp"
+#include "DecodeString394.hpp"
+#include "SurroundedRegions130.hpp"
+#include "PathSum113.hpp"
+#include "ZeroOneMatrix542.hpp"
+#include "PacificAtlanticWaterFlow417.hpp"
+#include "SumRootToLeafNum129.hpp"
+#include "FindBottomLeftTreeValue513.hpp"
+#include "ShoppingOffers638.hpp"
+int main()
+{
+  //NumOfIslands200 test; test.test();
+  //FriendCircles547 test; test.test();
+  //DecodeString394 test; test.test();
+  //SurroundedRegion130 test; test.test();
+  //PathSum113 test; test.test();
+  //ZeroOneMatrix542 test; test.test();
+  //WaterFlow417 test; test.test();
+  //SumRootToLeafNum129 test; test.test();
+  //FindBottomLeftTreeValue513 test; test.test();
+  ShoppingOffers638 test; test.test();
+}
