125526ef18e208a16df29f264e530827aaf69f06
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:45:11 +0000

    Create testht.cpp

---
+++ hashtable/testht.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <set>
+#include "utils.hpp"
+#include "LengthOfLongestSubStr3.hpp"
+#include "GroupAnagrams49.hpp"
+#include "ValidSudoku36.hpp"
+#include "TopKFreqElem347.hpp"
+int main()
+{
+ //LengthOfLongestSubStr3 test; test.test();
+ //GroupAnagrams49 test; test.test(); 
+  //ValidSudoku36 test; test.test();
+  TopKFreqElem347 test; test.test();
+}
