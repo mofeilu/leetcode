02387f72a7f5c6de2389de025e57473bc15bf749
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:26:26 +0000

    Create testStack.cpp

---
+++ Stack/testStack.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include "utils.hpp"
+using namespace std;
+#include "ValidParentheses20.hpp"
+#include "NextGreaterElement496.hpp"
+#include "SimplifyPath71.hpp"
+#include "BasicCalculator224.hpp"
+#include "EvalRPN150.hpp"
+#include "RmDupLett316.hpp"
+int main()
+{
+
+//ValidParentheses20 t; t.test();
+//NextGreaterElement496 t; t.test();
+//SimplifyPath71 t; t.test();
+//BasicCalculator224 t; t.test();  
+  //EvalRPN150 t; t.test();
+  RmDupLett316 t; t.test();
+}
