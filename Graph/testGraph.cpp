a5670b4cc3fa5626332c12358b0aa13e5e158eb7
mofeilu <33995927+mofeilu@users.noreply.github.com>
2018-12-09 18:32:11 +0000

    Create testGraph.cpp

---
+++ Graph/testGraph.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
+#include <set>
+#include "utils.hpp"
+#include "MinimumHeightTrees310.hpp"
+#include "NetworkDelayTime743.hpp"
+#include "CountComponents323.hpp"
+#include "FindEventualSafeStates802.hpp"
+#include "CouplesHoldingHands765.hpp"
+#include "CourseSchedule210.hpp"
+#include "SimilarStringGroups839.hpp"
+int main()
+{
+ //NetworkDelayTime743 test; test.test();	
+ //MinimumHeightTrees310 test; test.test();
+ //CountComponents323 test; test.test();
+ //FindEventualSafeStates802 test; test.test();
+ //CouplesHoldingHands765 test; test.test();
+ //CourseSchedule210 test; test.test();
+ SimilarStringGroups839 test;test.test();
+ 
+}
